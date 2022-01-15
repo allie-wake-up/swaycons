9da8498331c14bcc12c7d1f00346d2a8217286ee
Allie Stephan <allie@pointguard.dev>
2022-01-15 00:50:51 -0500

    Very rough code that does some of the basics
    - receives window events
    - runs commands to change title_format
    
    - TODO: figure out how to properly prevent an endless loop from changing
      title_format re-triggering the window events

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use swayipc::{Connection, Event, EventType, Fallible, WindowChange};
+use regex::RegexSet;
+
+fn main() -> Fallible<()> {
+    let set = RegexSet::new(&[
+        r"google\.com",
+    ]).unwrap();
+    let mut ignore = vec![];
+    let mut connection = Connection::new()?;
+    for event in Connection::new()?.subscribe([EventType::Window])? {
+        if let Event::Window(w) = event? {
+            if w.change == WindowChange::Title {
+                let id = w.container.id;
+                if let Some(app_id) = w.container.app_id {
+                    if let Some(name) = w.container.name {
+                        println!("{:?}", ignore);
+                        println!("id: {}, app_id: {}, name: {}", id, app_id, name);
+                        if let Some(index) = set.matches(name.as_str()).iter().next() {
+                            match ignore.iter().position(|&x: &i64| x == id) {
+                                Some(i) => {
+                                    ignore.swap_remove(i as usize);
+                                    ()
+                                },
+                                None => {
+                                    ignore.push(id);
+                                    ignore.push(id);
+                                    ignore.push(id);
+                                    connection.run_command("[title=\"google\\.com\"] title_format \"%title\"")?;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}

ed2a0f16b2cf05d679522bce0dbdc46892092b66
Allie Stephan <allie@pointguard.dev>
2022-02-12 14:06:22 -0500

    Refactor a little bit
    - also add more stuff to default config

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,15 @@ const DEBUG: bool = false;
 fn main() -> Fallible<()> {
     let settings = get_settings().unwrap();
     let global_settings = settings.global;
+    let global_base = BaseSettings {
+        color: Some(global_settings.color.clone()),
+        icon: Some(global_settings.icon.clone()),
+        size: Some(global_settings.size.clone()),
+    };
     let title_settings = settings.title;
     let app_id_settings = settings.app_id;
     let title_set = RegexSet::new(title_settings.keys()).unwrap();
-    let mut ignore: HashMap<i64, String> = HashMap::new();
+    let mut ignore: HashMap<i64, Option<String>> = HashMap::new();
     let mut last_focused = None;
     let mut last_focused_settings: Option<&BaseSettings> = None;
     let mut connection = Connection::new()?;
@@ -28,61 +33,58 @@ fn main() -> Fallible<()> {
             if DEBUG {
                 println!("id: {}, app_id: {}, name: {}", id, app_id, name);
             }
-            match find_best_match(&title_set, &name, &title_settings, &app_id) {
+            let mut ignore_matcher: Option<String> = None;
+            let settings = match find_best_match(&title_set, &name, &title_settings, &app_id) {
                 Some(index) => {
-                    if let Some(pattern) = title_set.patterns().get(index) {
-                        if ignore_entry != pattern || (focused && !(last_focused == Some(id))) {
-                            let settings = title_settings.get(pattern).unwrap();
-                            ignore.insert(id, pattern.to_owned());
-                            if focused {
-                                if let Some(last_id) = last_focused {
-                                    if let Some(last_settings) = last_focused_settings {
-                                        set_icon(
-                                            last_id,
-                                            last_settings,
-                                            &global_settings,
-                                            &mut connection,
-                                            false,
-                                        )?;
-                                    }
-                                }
-                                last_focused = Some(id);
-                                last_focused_settings = Some(&settings.base);
-                            }
-                            set_icon(
-                                id,
-                                &settings.base,
-                                &global_settings,
-                                &mut connection,
-                                focused,
-                            )?;
-                        }
+                    let pattern = title_set.patterns().get(index);
+                    if pattern.is_some() {
+                        ignore_matcher = match pattern {
+                            Some(p) => Some(p.to_string()),
+                            None => None
+                        };
+                        let settings = title_settings.get(pattern.unwrap()).unwrap();
+                        &settings.base
+                    } else {
+                        &global_base
                     }
                 }
                 None => {
-                    if let Some(settings) = app_id_settings.get(&app_id) {
-                        if ignore_entry != &app_id || (focused && !(last_focused == Some(id))) {
-                            ignore.insert(id, app_id);
-                            if focused {
-                                if let Some(last_id) = last_focused {
-                                    if let Some(last_settings) = last_focused_settings {
-                                        set_icon(
-                                            last_id,
-                                            last_settings,
-                                            &global_settings,
-                                            &mut connection,
-                                            false,
-                                        )?;
-                                    }
-                                }
-                                last_focused = Some(id);
-                                last_focused_settings = Some(&settings);
-                            }
-                            set_icon(id, settings, &global_settings, &mut connection, focused)?;
-                        }
+                    let settings = app_id_settings.get(&app_id);
+                    ignore_matcher = Some(app_id);
+                    if settings.is_some() {
+                        settings.unwrap()
+                    } else {
+                        &global_base
                     }
                 }
+            };
+            if ignore_matcher.is_some() && *ignore_entry != ignore_matcher
+                || (focused && !(last_focused == Some(id)))
+            {
+                if DEBUG {
+                    println!("setting icon for id: {}, focused: {}", id, focused);
+                }
+                set_icon(id, &settings, &global_settings, &mut connection, focused)?;
             }
+            if focused && Some(id) != last_focused {
+                if let Some(last_id) = last_focused {
+                    if let Some(last_settings) = last_focused_settings {
+                        if DEBUG {
+                            println!("id: {} lost focus", last_id);
+                        }
+                        set_icon(
+                            last_id,
+                            last_settings,
+                            &global_settings,
+                            &mut connection,
+                            false,
+                        )?;
+                    }
+                }
+                last_focused = Some(id);
+                last_focused_settings = Some(&settings);
+            }
+            ignore.insert(id, ignore_matcher);
         }
     }
     Ok(())
